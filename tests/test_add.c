f75044ff7329d8d29a62bbf8ceda46039fc30614
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-05-16 19:06:05 +0530

    Add unit tests for bigint addition

--- tests/test_add.c
+++ tests/test_add.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-char* s_add(char *n1_arr, char *n2_arr, int s_n1, int s_n2);
-
+char* s_add(char*, char*, int, int);
 
 int main()
 {
     char *result;
 
     result = s_add("999", "1", 3, 1);
+    printf("999 + 1 = %s\n", result);
+    free(result);
 
-    printf("%s\n", result);
+    result = s_add("0", "0", 1, 1);
+    printf("0 + 0 = %s\n", result);
+    free(result);
 
+    result = s_add("123", "456", 3, 3);
+    printf("123 + 456 = %s\n", result);
+    free(result);
+
+    result = s_add("999999", "1", 6, 1);
+    printf("999999 + 1 = %s\n", result);
     free(result);
 
     return 0;
