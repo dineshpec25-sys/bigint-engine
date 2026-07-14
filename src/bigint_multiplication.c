36ab19aa08e744566fc66e53c260234f3832910d
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-07-14 22:31:28 +0530

    Started multiplication

---
+++ src/bigint_multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>  
+
+char* bigint_multiplication(char *n1_arr, char *n2_arr, int s_n1, int s_n2);
+
+char* bigint_multiplication(char *n1_arr, char *n2_arr, int s_n1, int s_n2)
+{
+    int capacity = ( s_n1 > s_n2) ? s_n1 : s_n2 ;
+
+    
+}
